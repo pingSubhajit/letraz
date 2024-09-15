2cc414623c76286aef11b2a4f04a1afd69eaad0e
Subhajit Kundu <subha60kundu@gmail.com>
2024-09-15 13:14:20 +0530

    chore: Remove poster from intro video in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
 const LandingPage = () => {
 	return (
 		<main className="h-screen overflow-hidden flex justify-center items-center">
-			<video autoPlay muted loop poster="/banner.png" className="aspect-video">
+			<video autoPlay muted loop className="aspect-video">
 				<source src="/letraz-intro.mp4" type="video/mp4"/>
 				Your browser does not support the video tag. You can <a href="/letraz-intro.mp4">download the video</a> instead.
 			</video>
