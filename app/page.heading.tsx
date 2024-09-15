c41884034c87cff794ac3dbeca2f9022025df6f7
Subhajit Kundu <subha60kundu@gmail.com>
2024-09-15 21:06:25 +0530

    fix: Landing page title leading issue

--- app/page.heading.tsx
+++ app/page.heading.tsx
@@ -7,7 +7,7 @@ const LandingPageHeading = ({className}: {className?: string}) => {
 	return (
 		<AnimatePresence>
 			<motion.h1
-				className={cn('text-3xl md:text-4xl lg:text-5xl text-center max-w-[700px] lg:max-w-[900px] leading-snug font-medium', className)}
+				className={cn('text-3xl md:text-4xl lg:text-5xl text-center max-w-[700px] lg:max-w-[900px] lg:leading-snug font-medium', className)}
 				initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 6.5, duration: 0.7}}
 			>
 				Craft unique résumés tailored to every job application
