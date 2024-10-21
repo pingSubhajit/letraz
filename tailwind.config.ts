06a6db423d94dc5427dcc98006fb6162e0ac774a
Subhajit Kundu <subha60kundu@gmail.com>
2024-10-21 13:49:06 +0530

    chore: Fix lint error in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,11 +5,11 @@ const config: Config = {
 	content: [
 		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
 	],
 	theme: {
-	    extend: {
-	        colors: {
+		extend: {
+			colors: {
 				flame: {
 					50: '#FCC6BC',
 					100: '#FBB3A5',
@@ -57,27 +57,27 @@ const config: Config = {
 				input: 'hsl(var(--input))',
 				ring: 'hsl(var(--ring))',
 				chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		fontFamily: {
-  			sans: ['var(--font-geist-sans)'],
-  			mono: ['var(--font-geist-mono)']
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+					'1': 'hsl(var(--chart-1))',
+					'2': 'hsl(var(--chart-2))',
+					'3': 'hsl(var(--chart-3))',
+					'4': 'hsl(var(--chart-4))',
+					'5': 'hsl(var(--chart-5))'
+				}
+			},
+			fontFamily: {
+				sans: ['var(--font-geist-sans)'],
+				mono: ['var(--font-geist-mono)']
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		}
 	},
 	plugins: [
 		require('tailwindcss-animate'),
 		require('@tailwindcss/typography')
-	],
+	]
 }
 export default config
