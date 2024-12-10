4334ad0c00d749e7613301aa9dff49d18239b549
Sahil Verma <domainsahil007@gmail.com>
2024-12-10 08:02:28 +0530

    fix: responsiveness fixes

--- components/onboarding/ExperienceForm.tsx
+++ components/onboarding/ExperienceForm.tsx
@@ -280,7 +280,8 @@ const ExperienceForm = ({className, experiences, setExperiences}: ExperienceForm
 					</motion.div>
 
 					<div
-						className="w-full max-w-[95rem] flex items-center justify-between fixed left-1/2 -translate-x-1/2 z-10 bottom-16 px-16"
+						className="w-[calc(100%-4.7rem)] flex items-center justify-between fixed left-[4.7rem] z-10 bottom-16 px-16"
+
 					>
 						{/* PREVIOUS STEP BUTTON */}
 						<Link href={'/app/onboarding?step=education'}>
