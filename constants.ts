cd8dc669f68ed6660bbab0303a04f34c8ff872cd
Subhajit Kundu <subha60kundu@gmail.com>
2025-01-04 13:06:41 +0530

    chore: Replace ghost blog URL with production version

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 export const years = Array.from({length: 50}, (_, i) => (new Date().getFullYear() - i).toString())
 
-export const ghostBlogUrl = 'https://letraz-changelog.ghost.io'
+export const ghostBlogUrl = 'https://blog.letraz.app'
 
 export const defaultUrl = 'https://letraz.com'
 
