cb6ee6b912d9130e1db036790801ada010060245
Subhajit Kundu <subha60kundu@gmail.com>
2025-01-14 10:27:42 +0530

    fix: Posthog not capturing any events

--- components/providers/PosthogProvider.tsx
+++ components/providers/PosthogProvider.tsx
@@ -5,7 +5,7 @@ import {PostHogProvider} from 'posthog-js/react'
 import {ReactNode} from 'react'
 import {defaultUrl} from '@/config'
 
-if (typeof window !== 'undefined' && defaultUrl.match(/^.((localhost|\d+.\d+.\d+.\d+)|vercel.app).$/)) {
+if (typeof window !== 'undefined' && !defaultUrl.match(/(.(localhost|\d+.\d+.\d+.\d+).|.*vercel.app)/)) {
 	posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
 		api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 		person_profiles: 'always'
