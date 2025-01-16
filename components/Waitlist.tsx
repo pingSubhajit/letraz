fcd9e5127a0d260c3ce4881b74837354162fa93a
Sourabh Rathour <84420690+sourabhrathourr@users.noreply.github.com>
2025-01-16 21:46:58 +0530

    LET-71 | feat(waitlist): implement optimistic UI updates for signup (#86)
    * feat(waitlist): implement optimistic UI updates for signup
    * fix: Remove unnecessary states in light of an improved logic
    * refactor(waitlist): improve animation structure
    
    ---------
    
    Co-authored-by: Subhajit Kundu <subha60kundu@gmail.com>

--- components/Waitlist.tsx
+++ components/Waitlist.tsx
@@ -10,7 +10,6 @@ import {cn} from '@/lib/utils'
 import {signUpForWaitlist} from '@/lib/waitlist.methods'
 import {useState} from 'react'
 import {AnimatePresence, motion} from 'motion/react'
-import {Loader2} from 'lucide-react'
 import {toast} from 'sonner'
 import {discordHandle} from '@/config'
 
@@ -29,64 +28,68 @@ const Waitlist = ({className, referrer}: {className?: string, referrer: string |
 
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
+			setSignedUp(true)
 			await signUpForWaitlist(values.email, referrer)
 			form.reset()
-			setSignedUp(true)
 		} catch (error) {
+			setSignedUp(false)
 			toast.error('Failed to sign up, please try again')
 		}
 	}
 
 	return (
 		<div className={cn('', className)}>
-			<AnimatePresence>
-				{!signedUp && <motion.div
-					initial={{opacity: 0}}
-					animate={{opacity: 1}}
-					exit={{opacity: 0}}
-				>
-					<Form {...form}>
-						<form onSubmit={form.handleSubmit(onSubmit)} className="flex items-stretch gap-1 justify-between">
-							<FormField
-								control={form.control}
-								name="email"
-								render={({field}) => (
-									<FormItem className="relative">
-										<FormControl>
-											<Input
-												placeholder="Enter your email" {...field}
-												className="bg-neutral-50 peer w-full lg:w-96 rounded-l-full py-3 lg:py-4 h-auto"
-											/>
-										</FormControl>
-									</FormItem>
-								)}
-							/>
+			<AnimatePresence mode="wait">
+				{!signedUp ? (
+					<motion.div
+						key="form"
+						initial={{opacity: 0}}
+						animate={{opacity: 1}}
+						exit={{opacity: 0}}
+					>
+						<Form {...form}>
+							<form onSubmit={form.handleSubmit(onSubmit)} className="flex items-stretch gap-1 justify-between">
+								<FormField
+									control={form.control}
+									name="email"
+									render={({field}) => (
+										<FormItem className="relative">
+											<FormControl>
+												<Input
+													placeholder="Enter your email" {...field}
+													className="bg-neutral-50 peer w-full lg:w-96 rounded-l-full py-3 lg:py-4 h-auto"
+												/>
+											</FormControl>
+										</FormItem>
+									)}
+								/>
 
-							<Button
-								type="submit"
-								disabled={!form.formState.isValid || form.formState.isSubmitting}
-								className="rounded-r-full h-auto px-4 lg:px-8"
-							>
-								{form.formState.isSubmitting && <Loader2 className="w-4 h-4 animate-spin mr-2" />}
-								Join waitlist
-							</Button>
-						</form>
-					</Form>
-				</motion.div>}
-
-				{!signedUp && <motion.p
-					initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}}
-					className="mt-4 text-xs text-primary/60" >
-					Your email will not be shared with any third parties.
-				</motion.p>}
-
-				{signedUp && <motion.p
-					initial={{opacity: 0}}
-					animate={{opacity: 1}}
-					exit={{opacity: 0}}
-				>
-					Thanks for signing up! Join our <a href={discordHandle} target="_blank" className="text-flame-500 font-medium hover:underline focus-visible:underline">Discord</a> to stay connected.
-				</motion.p>}
+								<Button
+									type="submit"
+									disabled={!form.formState.isValid || form.formState.isSubmitting}
+									className="rounded-r-full h-auto px-4 lg:px-8"
+								>
+									Join waitlist
+								</Button>
+							</form>
+						</Form>
+						<p className="mt-4 text-xs text-primary/60">
+							Your email will not be shared with any third parties.
+						</p>
+					</motion.div>
+				) : (
+					<motion.div
+						key="success"
+						initial={{opacity: 0}}
+						animate={{opacity: 1}}
+						exit={{opacity: 0}}
+					>
+						Thanks for signing up! Join our <a href={discordHandle} target="_blank"
+							className="text-flame-500 font-medium hover:underline focus-visible:underline">
+							Discord
+						</a> to stay connected.
+					</motion.div>
+				)}
 			</AnimatePresence>
 		</div>
 	)
