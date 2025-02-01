6cf356e9e522e13770b78f534160c5ffbbc6742a
Subhajit Kundu <subha60kundu@gmail.com>
2025-02-01 11:07:48 +0530

    chore: Error handling for failed email send upon waitlist signup

--- lib/waitlist/actions.ts
+++ lib/waitlist/actions.ts
@@ -3,7 +3,7 @@
 import {Resend} from 'resend'
 import WaitlistWelcomeEmail from '@/emails/welcome'
 import {WaitlistMutationSchema} from '@/lib/waitlist/types'
-
+import * as Sentry from '@sentry/nextjs'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
@@ -24,13 +24,17 @@ export const signUpForWaitlist = async (email: string, referrer?: string) => {
 		return params
 	}
 
-	await resend.emails.send({
-		from: 'Subhajit from Letraz <subhajit@letraz.app>',
-		replyTo: 'Subhajit from Letraz <subhajit@letraz.app>',
-		to: params.email,
-		subject: 'Welcome to Letraz waitlist!',
-		react: WaitlistWelcomeEmail()
-	})
+	try {
+		await resend.emails.send({
+			from: 'Subhajit from Letraz <subhajit@letraz.app>',
+			replyTo: 'Subhajit from Letraz <subhajit@letraz.app>',
+			to: params.email,
+			subject: 'Welcome to Letraz waitlist!',
+			react: WaitlistWelcomeEmail()
+		})
+	} catch (error: any) {
+		Sentry.captureException(error)
+	}
 
 	return WaitlistMutationSchema.parse(await response.json())
 }
