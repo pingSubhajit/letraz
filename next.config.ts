9b1ed7b5ad39327c707a3c9cfe1fa1574b4d1d37
Subhajit Kundu <subha60kundu@gmail.com>
2025-02-01 11:20:14 +0530

    core: Remove DB operations & all signatures of database and related stuff

--- next.config.ts
+++ next.config.ts
@@ -11,11 +11,6 @@ const nextConfig: NextConfig = {
 }
 
 export default withSentryConfig(nextConfig, {
-	/*
-	 * For all available options, see:
-	 * https://github.com/getsentry/sentry-webpack-plugin#options
-	 */
-
 	org: 'letraz-app',
 	project: 'letraz-client',
 
@@ -30,12 +25,6 @@ export default withSentryConfig(nextConfig, {
 		deleteSourcemapsAfterUpload: true
 	},
 
-	/*
-	 * For all available options, see:
-	 * https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-	 */
-
-	// Upload a larger set of source maps for prettier stack traces (increases build time)
 	widenClientFileUpload: true,
 
 	// Automatically annotate React components to show their full name in breadcrumbs and session replay
@@ -43,12 +32,7 @@ export default withSentryConfig(nextConfig, {
 		enabled: true
 	},
 
-	/*
-	 * Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-	 * This can increase your server load as well as your hosting bill.
-	 * Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-	 * side errors will fail.
-	 */
+	// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
 	tunnelRoute: '/monitoring',
 
 	// Hides source maps from generated client bundles
@@ -57,11 +41,6 @@ export default withSentryConfig(nextConfig, {
 	// Automatically tree-shake Sentry logger statements to reduce bundle size
 	disableLogger: true,
 
-	/*
-	 * Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-	 * See the following for more information:
-	 * https://docs.sentry.io/product/crons/
-	 * https://vercel.com/docs/cron-jobs
-	 */
+	// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
 	automaticVercelMonitors: true
 })
