4c7adcf939a71c3aa94fed44c5227aadba63a5d4
Subhajit Kundu <subha60kundu@gmail.com>
2025-03-20 21:50:07 +0530

    chore: initial preparation for implementing loading state

---
+++ components/resume/themes/DEAFULT_THEME/DefaultThemeSkeleton.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+const DefaultThemeSkeleton = () => {
+	return (
+		<div className="h-full p-12 overflow-y-auto">
+			<div className="flex flex-col gap-3 items-center">
+
+			</div>
+		</div>
+	)
+}
+
+export default DefaultThemeSkeleton
