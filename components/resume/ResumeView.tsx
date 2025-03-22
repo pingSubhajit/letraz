68c17c06af408eea29e40ae08d0beb8e73d3e86e
Sahil Verma <83828231+sahilverma-dev@users.noreply.github.com>
2025-03-22 09:15:43 -0700

    LET-89 | logic: Convert BaseResume in the onboarding to client side data fetching & revalidation (#118)
    * feat(education): hook up api for the education editor
    * fix: add education from EducationEditor not working
    * chore: remove unnecessary console log from response handling
    * fix: "current" checkbox not working for EducationEditor
    * feat(education): add update education functionality in EducationEditor
    * feat(resume): implement prefetching of base resume data using React Query
    * refactor: rename baseResultQueryOptions to baseResumeQueryOptions and revalidate properly from editor
    
    * chore: remove unnecessary OpenAPI spec file
    
    ---------
    
    Co-authored-by: Sourabh <rathoursourabh5@gmail.com>

--- components/resume/ResumeView.tsx
+++ components/resume/ResumeView.tsx
@@ -1,23 +1,30 @@
 'use client'
 
 import ResumeEditor from '@/components/resume/ResumeEditor'
-import {useRef, useState} from 'react'
+import {useRef} from 'react'
 import dynamic from 'next/dynamic'
-import {Resume} from '@/lib/resume/types'
+import {useBaseResume} from '@/lib/resume/queries'
 
 const ResumeViewer = dynamic(() => import('@/components/resume/ResumeViewer'), {ssr: false})
 
-const ResumeView = ({resume}: {resume: Resume}) => {
+const ResumeView = () => {
 	const resumeRef = useRef<HTMLDivElement>(null)
 
-	const [resumeData, setResumeData] = useState<Resume>(resume)
+	const {data: resume, isLoading, isError} = useBaseResume()
+
 
 	return (
 		<div className="flex h-screen" role="main">
-			<div className="shadow-2xl bg-neutral-50 size-a4 max-h-screen relative">
-				<ResumeViewer resumeRef={resumeRef} resume={resumeData} className="max-h-screen" />
+
+			{/* TODO component for loading and error handing */}
+			{isLoading && <p>loading</p>}
+			{isError && <p>loading</p>}
+			{ resume && <> <div className="shadow-2xl bg-neutral-50 size-a4 max-h-screen relative">
+				<ResumeViewer resumeRef={resumeRef} resume={resume} className="max-h-screen" />
 			</div>
 			<ResumeEditor resume={resume} className="size-full bg-neutral-50 p-12" />
+			</>
+			}
 		</div>
 	)
 }
