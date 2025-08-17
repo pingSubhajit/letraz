6add4c9c9a20421caf61252ab6cb8f2cc1fdb212
Sourabh Rathour <rathoursourabh5@gmail.com>
2025-08-17 13:41:06 +0530

    fix: fix linter issues

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -28,12 +28,12 @@ type CommandDialogProps = DialogProps & { title?: string; description?: string }
 const CommandDialog = ({children, title = 'Search', description, ...props}: CommandDialogProps) => {
 	return (
 		<Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg">
-        {/* Visually hidden accessible name/description for screen readers */}
-        <DialogTitle className="sr-only">{title}</DialogTitle>
-        {description ? (
-          <DialogDescription className="sr-only">{description}</DialogDescription>
-        ) : null}
+			<DialogContent className="overflow-hidden p-0 shadow-lg">
+				{/* Visually hidden accessible name/description for screen readers */}
+				<DialogTitle className="sr-only">{title}</DialogTitle>
+				{description ? (
+					<DialogDescription className="sr-only">{description}</DialogDescription>
+				) : null}
 				<Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
 					{children}
 				</Command>
