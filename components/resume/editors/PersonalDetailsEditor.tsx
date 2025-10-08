373e47d649608c960adfb0376824d21999f36913
Subhajit Kundu <subha60kundu@gmail.com>
2025-10-08 10:20:13 +0530

    feat: disable ability to update user's email

--- components/resume/editors/PersonalDetailsEditor.tsx
+++ components/resume/editors/PersonalDetailsEditor.tsx
@@ -269,7 +269,7 @@ const PersonalDetailsEditor: React.FC<Props> = ({className, isTabSwitch = false}
 										name="email"
 										label="Email"
 										placeholder="e.g. john.smith@email.com"
-										disabled={isSubmitting}
+										disabled={true}
 									/>
 
 									<TextFormField
