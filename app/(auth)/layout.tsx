0c7de6903a6808375d61ded4fe3c18a03f944d24
Subhajit Kundu <subha60kundu@gmail.com>
2025-10-27 19:08:34 +0530

    fi: auth page title responsiveness for smaller desktop screens

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -11,15 +11,15 @@ const AuthLayout = ({children}: AuthLayoutProps) => {
 			<WebsiteNavBar className="absolute top-0 left-0 w-full z-50" />
 
 			<div className="h-screen overflow-hidden flex justify-center lg:justify-end items-center w-screen">
-			<div className="absolute inset-0 overflow-hidden -z-10">
-				<video autoPlay muted loop className="aspect-video absolute -z-10 w-full h-full scale-125 lg:scale-150 blur-md">
-					<source src="/brain-pulse.webm" type="video/webm"/>
-				</video>
+				<div className="absolute inset-0 overflow-hidden -z-10">
+					<video autoPlay muted loop className="aspect-video absolute -z-10 w-full h-full scale-125 lg:scale-150 blur-md">
+						<source src="/brain-pulse.webm" type="video/webm"/>
+					</video>
+				</div>
+				<div className="w-full overflow-y-auto max-w-lg lg:w-[50vw] lg:max-w-none h-screen sm:h-auto sm:max-h-[90vh] sm:rounded-3xl lg:h-screen lg:max-h-none lg:rounded-none bg-neutral-50 flex flex-col justify-center items-start shadow-2xl p-6 sm:p-8 md:p-12 lg:p-24 xl:p-28 2xl:p-48">
+					{children}
+				</div>
 			</div>
-			<div className="w-full max-w-lg lg:w-[50vw] lg:max-w-none h-screen sm:h-auto sm:max-h-[90vh] sm:rounded-3xl lg:h-screen lg:max-h-none lg:rounded-none bg-neutral-50 flex flex-col justify-center items-start shadow-2xl p-6 sm:p-8 md:p-12 lg:p-24 xl:p-28 2xl:p-48">
-				{children}
-			</div>
-		</div>
 		</>
 	)
 }
